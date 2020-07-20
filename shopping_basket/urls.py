ba4e98943d9e123fd328bbd55da8a08a718e61ce
stellaexp <kimshillam@icloud.com>
2020-07-20 07:24:46 +0000

    script and html strcuture for edit and delete basket functionality

--- shopping_basket/urls.py
+++ shopping_basket/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_bag, name='view_bag'),
-    path('add/<item_id>/', views.add_to_basket, name='add_to_basket'),
+    path('', views.view_bag, name = 'view_bag'),
+    path('add/<item_id>/', views.add_to_basket, name = 'add_to_basket'),
+    path('edit/<item_id>/', views.edit_basket, name = 'edit_basket'),
+    path('delete_item_from_basket/<item_id>/', views.delete_item_from_basket, name = 'delete_item_from_basket'),
 ]
--- shopping_basket/views.py
+++ shopping_basket/views.py
@@ -8,7 +8,7 @@ def view_bag(request):
 
 
 def add_to_basket(request, item_id):
-    """ Lets user add quantity of products to their shopping bag """
+    """ Lets user add quantity of products to their shopping basket """
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
@@ -20,5 +20,35 @@ def add_to_basket(request, item_id):
         bag[item_id] = quantity
 
     request.session['bag'] = bag
+
+    return redirect(redirect_url)
+
+
+def edit_basket(request, item_id):
+    """ Lets user update quantity in their shopping basket """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] = quantity
+
+    request.session['bag'] = bag
+
+    return redirect(redirect_url)
+
+
+def delete_item_from_basket(request, item_id):
+    """ View to delete item from users shopping basket """
+
+    # Looked up how to delete item from basket here
+    # https://stackoverflow.com/questions/52477268/how-to-remove-an-item-from-django-session
+
+    bag = request.session.get('bag', {})
+    del bag[item_id]
+    request.session['bag'] = bag
+
+    redirect_url = request.POST.get('redirect_url')
 
     return redirect(redirect_url)
