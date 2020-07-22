8f7d26414cb384943ad34bf8cb3e320b0d78b038
stellaexp <kimshillam@icloud.com>
2020-07-22 17:28:37 +0000

    update urls, settings and requirements.txt

--- mysite/settings.py
+++ mysite/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'products',
     'shopping_basket',
     'checkout',
+    # other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -58,6 +60,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'mysite.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -75,6 +79,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'shopping_basket.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
