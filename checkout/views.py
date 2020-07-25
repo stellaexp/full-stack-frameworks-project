efbff2bfa94678d4e4b4b3f61ce944dc10c87725
stellaexp <kimshillam@icloud.com>
2020-07-25 09:57:51 +0000

    add stripe functionality

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51H8Lv8JY9BftfxITI4p6ea6WQicFAA51SEG2mfmejFaP28Lzosvo8WcQuUKTFBxPK8nSVoUfFG5QGUSPWRHku0R000V9mnx2YR',
+        'client_secret_key': 'test client secret',
     }
 
     return render(request, template, context)
