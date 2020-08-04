a45e54dc9c80311f71f8c6985f88153ab12116ee
stellaexp <kimshillam@icloud.com>
2020-08-04 07:48:24 +0000

    update stripe js script for webhook handling and urls

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -28,28 +28,59 @@ card.addEventListener('change', function (event) {
 var form = document.getElementById('payment-form');
 
 form.addEventListener('submit', function(ev) {
-  ev.preventDefault();
-  stripe.confirmCardPayment(clientSecret, {
-    payment_method: {
-        card: card,
-      }
-  }).then(function(result) {
-    if (result.error) {
-      // Shows error to customer 
-      var errorDiv = document.getElementById('card-errors');
-            var html = `
-                <span class="icon" role="alert">
-                <i class="fas fa-times"></i>
-                </span>
-                <span>${result.error.message}</span>`;
-            $(errorDiv).html(html);
-            card.update({ 'disabled': false});
-            $('#submit-btn').attr('disabled', false);
-    } else {
-      // The payment has been processed!
-      if (result.paymentIntent.status === 'succeeded') {
-            form.submit();
-      }
-    }
-  });
-});
+	    ev.preventDefault();
+	    card.update({ 'disabled': true});
+        $('#submit-btn').attr('disabled', true);
+        
+	    var saveInfo = Boolean($('#id-save-info').attr('checked'));
+	    // From using {% csrf_token %} in the form
+	    var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
+	    var postData = {
+	        'csrfmiddlewaretoken': csrfToken,
+	        'client_secret': clientSecret,
+	        'save_info': saveInfo,
+	    };
+	    var url = '/checkout/cache_checkout/';
+	
+	    $.post(url, postData).done(function() {
+	        stripe.confirmCardPayment(clientSecret, {
+	            payment_method: {
+	                card: card,
+	                billing_details: {
+	                    name: $.trim(form.full_name.value), 
+	                    phone: $.trim(form.telephone.value),
+	                    email: $.trim(form.email.value),
+	                    address:{
+	                        line1: $.trim(form.address1.value),
+                            line2: $.trim(form.address2.value),
+                            city: $.trim(form.county.value),
+	                    }
+	                }
+	            },
+	            shipping: {
+	                name: $.trim(form.full_name.value),
+	                address: {
+	                    line1: $.trim(form.address1.value),
+                        line2: $.trim(form.address2.value),
+	                    city: $.trim(form.county.value),
+	                    postal_code: $.trim(form.postcode.value),
+	                }
+	            },
+	        }).then(function(result) {
+	            if (result.error) {
+	                var errorDiv = document.getElementById('card-errors');
+	                var html = `
+	                    <span class="icon" role="alert">
+	                    <i class="fa fa-times"></i>
+	                    </span>
+	                    <span>${result.error.message}</span>`;
+	                $(errorDiv).html(html)
+	                $('#submit-btn').attr('disabled', false);
+	            } else {
+	                if (result.paymentIntent.status === 'succeeded') {
+	                    form.submit();
+	                }
+	            }
+	        });
+	    })
+	});
--- checkout/urls.py
+++ checkout/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
 
+from .webhooks import webhook
+
+
 urlpatterns = [path('', views.checkout, name='checkout'),
                path('checkout_success/<order_number>',
-               views.checkout_success, name='checkout_success')]
+               views.checkout_success, name='checkout_success'),
+               path('cache_checkout/', views.cache_checkout,
+               name='cache_checkout'), path('wh/', webhook,
+               name='webhook')]
